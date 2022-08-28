8f0a5e5531504b0e7a67f0d1053a29867ceff309
Vanessa Winklbauer <winklbauer.vanessa.marie@gmail.com>
2022-08-28 13:25:18 +0200

    Update model code to use specific confounding vars

--- analyze/analyze_top_words.py
+++ analyze/analyze_top_words.py
@@ -3,17 +3,26 @@ import glob
 from nltk.stem.snowball import SnowballStemmer
 
 path = 'csvs/label/*'
-all_files = glob.glob(path + "/*top_seller.csv")
 
+
+# brand
+# price brand
+# price brand category
+
+file_names = ['brand price', 'brand price category_id']
 li = []
 
-for filename in all_files:
-    if any([x in filename for x in ['lower', 'stopped', 'stemmed']]):
-        print("found")
-    else:
-        continue
-    df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
-    li.append(df)
+for f in file_names:
+    all_files = glob.glob(path + "/"+f+"_top_seller.csv")
+
+    for filename in all_files:
+        if any([x in filename for x in ['lower','stopped']]):
+            print(filename)
+        else:
+            continue
+
+        df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
+        li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
@@ -21,17 +30,26 @@ df.to_pickle("classification_count.pkl")
 
 
 path = 'csvs/label_2/*'
-all_files = glob.glob(path + "/*top_seller.csv")
+
+# price brand
+# category preis
+# price brand category
+
+file_names = ['brand price', 'brand price category_id']
 
 li = []
 
-for filename in all_files:
-    if any([x in filename for x in ['lower', 'stopped', 'stemmed']]):
-        print("found")
-    else:
-        continue
-    df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
-    li.append(df)
+for f in file_names:
+    all_files = glob.glob(path + "/"+f+"_top_seller.csv")
+
+    for filename in all_files:
+        if any([x in filename for x in ['lower','stopped']]):
+           print(filename)
+        else:
+            continue
+
+        df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
+        li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
@@ -40,21 +58,22 @@ df.to_pickle("classification2_count.pkl")
 
 
 path = 'csvs/mean_revenue/*'
-all_files = glob.glob(path + "/*.csv")
+all_files = glob.glob(path + "/brand price.csv")
 path = 'csvs/median_revenue/*'
-all_files = all_files + glob.glob(path + "/*.csv")
+all_files = all_files + glob.glob(path + "/brand price category_id.csv")
 
 li = []
 
 for filename in all_files:
-    if any([x in filename for x in ['lower', 'stopped', 'stemmed']]):
+    if any([x in filename for x in ['stopped', 'lower']]):
         print("found")
     else:
         continue
 
     if 'ohne' in filename:
-        print("ohne")
         continue
+
+    print(filename)
     df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
     li.append(df)
 
@@ -62,18 +81,14 @@ df = pd.concat(li, axis=0, ignore_index=True)
 
 df.to_pickle("mean_count.pkl")
 
-#df = pd.read_pickle("test.pkl")
-df1 = pd.read_pickle("../classification_count.pkl")
-df2 = pd.read_pickle("../classification2_count.pkl")
-df3 = pd.read_pickle("../mean_count.pkl")
+df1 = pd.read_pickle("classification_count.pkl")
+df2 = pd.read_pickle("classification2_count.pkl")
+#df3 = pd.read_pickle("mean_count.pkl")
 
 df = df1.append(df2)
 #df = df.append(df3)
 
-df['Wort'] = df['Wort'].apply(lambda x: x.lower())
-
-print(df)
 col = df.columns[1]
-df_new = df.groupby(['Wort'])[col].agg('sum').nlargest(51)
-
+df_new = df.groupby(['Wort'])[col].agg('sum').nlargest(65)
+pd.set_option('display.max_rows', None)
 print(df_new)
--- analyze/analyze_top_words_no_confounds.py
+++ analyze/analyze_top_words_no_confounds.py
@@ -2,8 +2,27 @@ import pandas as pd
 import glob
 from nltk.stem.snowball import SnowballStemmer
 
-path = 'csvs/label/*'
-all_files = glob.glob(path + "/*non_seller.csv")
+path = '../csvs/label/*'
+all_files = glob.glob(path + "/ohne_confound_top_seller.csv")
+
+li = []
+
+for filename in all_files:
+    if any([x in filename for x in ['lower', 'stopped']]):
+        print("found")
+        print(filename)
+    else:
+        continue
+    df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
+    li.append(df)
+
+df = pd.concat(li, axis=0, ignore_index=True)
+
+df.to_pickle("no_c_classification_count.pkl")
+
+
+path = '../csvs/label_2/*'
+all_files = glob.glob(path + "/ohne_confound_top_seller.csv")
 
 li = []
 
@@ -12,51 +31,24 @@ for filename in all_files:
         print("found")
     else:
         continue
-
-    if 'ohne_confound' in filename:
-        continue
-
     df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
     li.append(df)
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
-df.to_pickle("classification_count.pkl")
-
-
-path = 'csvs/label_2/*'
-all_files = glob.glob(path + "/*non_seller.csv")
-
-li = []
-
-for filename in all_files:
-    if any([x in filename for x in ['lower', 'stopped']]):
-        print("found")
-    else:
-        continue
-
-    if 'ohne_confound' in filename:
-        continue
-
-    df = pd.read_csv(filename, index_col=None, header=0, sep=';', encoding='latin-1')
-    li.append(df)
+df.to_pickle("no_c_classification2_count.pkl")
 
 df = pd.concat(li, axis=0, ignore_index=True)
 
-df.to_pickle("classification2_count.pkl")
+df.to_pickle("mean_count.pkl")
 
-
-df1 = pd.read_pickle("classification_count.pkl")
-df2 = pd.read_pickle("classification2_count.pkl")
+df1 = pd.read_pickle("no_c_classification_count.pkl")
+df2 = pd.read_pickle("no_c_classification2_count.pkl")
 
 df = df1.append(df2)
 
-#df['Wort'] = df['Wort'].apply(lambda x: x.lower())
-
-stemmer = SnowballStemmer('german')
-#df['Wort'] = df['Wort'].apply(lambda x: stemmer.stem(x))
-
+print(df)
 col = df.columns[1]
-df_new = df.groupby(['Wort'])[col].agg('sum').nlargest(60)
+df_new = df.groupby(['Wort'])[col].agg('sum').nlargest(59)
 
 print(df_new)
